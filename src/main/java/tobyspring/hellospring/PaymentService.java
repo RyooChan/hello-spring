2016dee946daaaa91f6107d35330cbb61d4f9a32
= <=>
2024-08-04 18:19:49 +0900

    user change

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class PaymentService {
+    // test
     private final SimpleExRateProvider exRateProvider;
 
     public PaymentService() {
