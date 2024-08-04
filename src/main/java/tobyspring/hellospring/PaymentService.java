a61aacd7f1e85a075c303c760273f05639ef2f79
= <=>
2024-08-04 18:31:07 +0900

    user change

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class PaymentService {
-    // test
+
     private final SimpleExRateProvider exRateProvider;
 
     public PaymentService() {
