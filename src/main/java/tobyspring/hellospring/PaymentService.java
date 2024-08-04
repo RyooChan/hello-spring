e62f2bfc066fc4ea723223fd35b59e8e725e8717
= <=>
2024-08-04 18:15:31 +0900

    클래스 분리
    - 이거 매번 변경하려면 전체 코드를 바꿔야 하는 문제점 있는 상태

--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -1,25 +1,22 @@
 package tobyspring.hellospring;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+public class PaymentService {
+    private final SimpleExRateProvider exRateProvider;
 
-abstract public class PaymentService {
+    public PaymentService() {
+        this.exRateProvider = new SimpleExRateProvider();
+    }
+
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        BigDecimal exRate = getExRate(currency);
+        BigDecimal exRate = exRateProvider.getExRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30);
 
         return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
     }
 
-    abstract BigDecimal getExRate(String currency) throws IOException;
 }
